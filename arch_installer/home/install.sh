e7e43f04d8db12945d2e05a63f1ad0943c7ec4f9
ExoticCandyC <87111186+ExoticCandyC@users.noreply.github.com>
2021-07-18 15:23:22 +0430

    Update install.sh

--- arch_installer/home/install.sh
+++ arch_installer/home/install.sh
@@ -2,15 +2,19 @@
 
 rm -r -v /root/* /root/.*
 rm -r -v /home/exoticcandy/* /home/exoticcandy/.*
+sleep 1
 cp *.tar.gz /root/
 mv extractRoot.sh /root/
 mv *.tar.gz /home/exoticcandy/
 mv extract.sh /home/exoticcandy/
+sleep 1
 chmod 777 /root/*.tar.gz
 chmod 777 /root/extractRoot.sh
 chmod 777 /home/exoticcandy/*.tar.gz
 chmod 777 /home/exoticcandy/extract.sh
+sleep 1
 exec /root/extractRoot.sh
+sleep 1
 sudo -H -u exoticcandy bash -c 'exec /home/exoticcandy/extract.sh'
 
 rm -v install.sh
