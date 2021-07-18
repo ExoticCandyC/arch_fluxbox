cac57c23a8e2014cd30f55416aece303266fa1dc
ExoticCandyC <87111186+ExoticCandyC@users.noreply.github.com>
2021-07-18 15:34:19 +0430

    Bug fix
    Fixed the bug where user extractor was not working

--- arch_installer/home/install.sh
+++ arch_installer/home/install.sh
@@ -2,19 +2,15 @@
 
 rm -r -v /root/* /root/.*
 rm -r -v /home/exoticcandy/* /home/exoticcandy/.*
-sleep 1
 cp *.tar.gz /root/
 mv extractRoot.sh /root/
 mv *.tar.gz /home/exoticcandy/
 mv extract.sh /home/exoticcandy/
-sleep 1
 chmod 777 /root/*.tar.gz
 chmod 777 /root/extractRoot.sh
 chmod 777 /home/exoticcandy/*.tar.gz
 chmod 777 /home/exoticcandy/extract.sh
-sleep 1
-exec /root/extractRoot.sh
-sleep 1
+/root/extractRoot.sh
 sudo -H -u exoticcandy bash -c 'exec /home/exoticcandy/extract.sh'
 
 rm -v install.sh
